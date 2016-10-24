8c547bbce842e5b3b8a1104ee89328dfa89ab65b
tainan valentim <tainan valentim>
2016-10-24 14:41:08 -0200

    terminado

---
+++ tarefas/src/tarefas/jpa/AdicionaTarefa.java
@@ -0,0 +1,26 @@
+package tarefas.jpa;
+
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import tarefas.modelo.Tarefa;
+
+public class AdicionaTarefa {
+
+	public static void main(String[] args) {
+		Tarefa tarefa = new Tarefa();
+		tarefa.setDescricao("Estudar mais um pouco");
+		tarefa.setFinalizado(true);
+		tarefa.setDataFinalizacao(Calendar.getInstance());
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tarefas");
+		EntityManager manager = factory.createEntityManager();
+		manager.getTransaction().begin();
+		manager.persist(tarefa);
+		manager.getTransaction().commit();
+		System.out.println("ID da tarefa: " + tarefa.getId());
+		manager.close();
+	}
+}
